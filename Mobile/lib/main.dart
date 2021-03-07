4736b7a7ae05256c6152228f7c13a17c90671e26
Kaythi <katherine.yeo09@gmail.com>
2021-03-07 18:29:15 +0800

    commit

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_splitwise/Components/expense.dart';
 import 'package:flutter_splitwise/Components/main_tab_bar_view.dart';
 import 'Components/sidebar.dart';
 import 'Components/main_tab_bar.dart';
@@ -13,35 +14,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Splitwise',
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color.fromRGBO(59, 174, 142, 1),
-            title: Text('SplitWise'),
-            actions: <Widget>[
-              IconButton(icon: const Icon(Icons.search), onPressed: null),
-              PopupMenuButton(
-                  icon: Icon(Icons.more_vert),
-                  itemBuilder: (context) => [
-                        PopupMenuItem(
-                          child: Text('Add friends on Splitwise'),
-                        ),
-                        PopupMenuItem(
-                          child: Text('Create a group'),
-                        ),
-                      ]),
-            ],
-            bottom: MainTabBar(),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            backgroundColor: Colors.orange[800],
-            onPressed: () {},
-          ),
-          drawer: Sidebar(),
-          body: MainTabBarView(),
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(59, 174, 142, 1),
+          title: Text('SplitWise'),
+          actions: <Widget>[
+            IconButton(icon: const Icon(Icons.search), onPressed: null),
+            PopupMenuButton(
+                icon: Icon(Icons.more_vert),
+                itemBuilder: (context) => [
+                      PopupMenuItem(
+                        child: Text('Add friends on Splitwise'),
+                      ),
+                      PopupMenuItem(
+                        child: Text('Create a group'),
+                      ),
+                    ]),
+          ],
+          bottom: MainTabBar(),
         ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.orange[800],
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Expense(),
+                ));
+          },
+        ),
+        drawer: Sidebar(),
+        body: MainTabBarView(),
       ),
     );
   }
